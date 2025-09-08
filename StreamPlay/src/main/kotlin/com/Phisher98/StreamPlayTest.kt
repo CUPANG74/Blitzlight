9d39d1ecda0c8d75ce03e5c56945e663287a0fe2
CUPANG74 <cupangmerah74@gmail.com>
2025-09-08 23:20:15 +0800

    Add files via upload

---
+++ StreamPlay/src/main/kotlin/com/Phisher98/StreamPlayTest.kt
@@ -0,0 +1,35 @@
+package com.phisher98
+
+
+import android.content.SharedPreferences
+import com.phisher98.StreamPlayExtractor.invokeVidSrcXyz
+import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.argamap
+import com.lagradost.cloudstream3.runAllAsync
+import com.lagradost.cloudstream3.utils.AppUtils
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.phisher98.StreamPlayExtractor.invokeDotmovies
+import com.phisher98.StreamPlayExtractor.invokeMovieBox
+import com.phisher98.StreamPlayExtractor.invokeMoviesdrive
+import com.phisher98.StreamPlayExtractor.invokeWatch32APIHQ
+import com.phisher98.StreamPlayExtractor.invokecatflix
+import com.phisher98.StreamPlayExtractor.invokemorph
+
+class StreamPlayTest(sharedPreferences:SharedPreferences?=null) : StreamPlay(sharedPreferences) {
+    override var name = "StreamPlay-Test"
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        val res = AppUtils.parseJson<LinkData>(data)
+        runAllAsync(
+            {
+                invokecatflix(res.id, res.epid, res.title, res.episode, res.season, callback)
+            },
+        )
+        return true
+    }
+
+}
