9fc59664da8981dfcb2634416438b3eadc95bc58
CUPANG74 <cupangmerah74@gmail.com>
2025-09-09 19:34:27 +0800

    Pusatfilm.kt

--- Pusatfilm/src/main/kotlin/com/pusatfilm/Pusatfilm.kt
+++ Pusatfilm/src/main/kotlin/com/pusatfilm/Pusatfilm.kt
@@ -13,7 +13,7 @@ import org.jsoup.nodes.Element
 
 class Pusatfilm : MainAPI() {
 
-    override var mainUrl = "http://pf21.net/"
+    override var mainUrl = "https://pf21.net/"
 
     override var name = "Pusatfilm"
     override val hasMainPage = true
