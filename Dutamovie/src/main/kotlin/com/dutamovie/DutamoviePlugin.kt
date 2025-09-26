ee5c8e6d55aacfb38d64338eb37fc4543cd7944f
CUPANG74 <cupangmerah74@gmail.com>
2025-09-26 17:24:02 +0800

    Add files via upload

---
+++ Dutamovie/src/main/kotlin/com/dutamovie/DutamoviePlugin.kt
@@ -0,0 +1,14 @@
+package com.dutamovie
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class DutaMoviePlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(DutaMovie())
+        registerExtractorAPI(Dingtezuni())
+      
+    }
+}
