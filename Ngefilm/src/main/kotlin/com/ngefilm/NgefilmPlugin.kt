534b6f2a3d010c3e59da506e84b21e70e78c1808
CUPANG74 <cupangmerah74@gmail.com>
2025-10-07 20:29:51 +0800

    Add files via upload

---
+++ Ngefilm/src/main/kotlin/com/ngefilm/NgefilmPlugin.kt
@@ -0,0 +1,19 @@
+package com.ngefilm
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class NgefilmPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(Ngefilm())
+        registerExtractorAPI(Dingtezuni())
+        registerExtractorAPI(Bingezove())
+        registerExtractorAPI(Mivalyo())
+        registerExtractorAPI(Movearnpre())
+        registerExtractorAPI(Dhtpre())
+        registerExtractorAPI(Hglink())
+    }
+}
+
